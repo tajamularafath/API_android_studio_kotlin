95a5d47f1e2059ecab7bb95567aab748cd6d7341
Tajamul Arafath <vptajju@gmail.com>
2024-05-14 17:06:47 +0530

    Added Retrofit/API

--- app/src/main/java/com/example/api/MainActivity.kt
+++ app/src/main/java/com/example/api/MainActivity.kt
@@ -2,10 +2,50 @@ package com.example.api
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val retrofitBuilder = Retrofit.Builder()
+            .baseUrl("https://dummyjson.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiInterface::class.java)
+
+        val retrofitData = retrofitBuilder.getProductData()
+
+
+        retrofitData.enqueue(object : Callback<MyData?> {
+            override fun onResponse(call: Call<MyData?>, response: Response<MyData?>) {
+                var responseBody = response.body()
+                val productList = responseBody?.products!!
+
+                val collectDataInSB = StringBuilder()
+
+                for(myData in productList){
+                    collectDataInSB.append(myData.title + ", ")
+                }
+
+                val tv = findViewById<TextView>(R.id.tv)
+                tv.setText(collectDataInSB)
+            }
+
+            override fun onFailure(call: Call<MyData?>, t: Throwable) {
+                Log.d("Main Activity", "OnFailure" + t.message)
+            }
+        })
+
     }
 }
